35ad3277789357db772b3b0e141b9925fda4e574
roberta <rt.tramontina@gmail.com>
2025-10-08 20:13:53 +0100

    initial commit

---
+++ python606/s3_ex_assincrono.py
@@ -0,0 +1,7 @@
+idade = int(input("Informe a sua idade: "))
+
+if idade >= 18:
+    print("Acesso autorizado")
+
+else:
+    print("Acesso não autorizado")
---
+++ python606/s3_ex_sincrono.py
@@ -0,0 +1,8 @@
+utilizador = input("Nome de utilizador: ")
+password = input("Password: ")
+
+if utilizador == "admin" and password == "1234":
+    print("Acesso permitido")
+else:
+    print("Acesso negado")
+    
